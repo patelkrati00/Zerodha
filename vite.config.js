a0d6c2ca39419271e398bb5c0bddef1c7010fe57
patelkrati00 <patelkrati00@gmail.com>
2025-08-12 19:11:14 +0530

    Initial setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
+
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        /* Your manifest.json content */
+      }
+    })],
 })
