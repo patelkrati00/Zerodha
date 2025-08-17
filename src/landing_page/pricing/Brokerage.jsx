6f37f5523f989892d133e75b7544a12676f97172
patelkrati00 <patelkrati00@gmail.com>
2025-08-17 19:13:28 +0530

    Pricing

--- src/landing_page/pricing/Brokerage.jsx
+++ src/landing_page/pricing/Brokerage.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
 
 function Brokerage() {
-    return ( 
-        <>
-         <h1>Brokerage</h1>
-        </>
-     );
+  return (
+    <>
+      <div className="container mb-5 border-bottom">
+        <div className="row border-top ">
+          <div className="col-8 p-4 mt-5 ">
+            <a href="" style={{textDecoration:"none"}}>
+              <h3 className="text-center">Brokerage Calculator</h3>
+            </a>
+            <ul className="text-muted p-5 " style={{lineHeight:"2.5", fontSize:"1.2rem"}}>
+              <li>
+                Our brokerage structure includes additional charges of ₹50 + GST
+                per order for Call & Trade and RMS auto-squareoff services.
+              </li>
+
+              <li>
+                Digital contract notes are provided free of charge via email,
+                while physical copies are available for ₹20 per contract note
+                plus courier charges.
+              </li>
+
+              <li>
+                For NRI accounts, non-PIS equity orders are charged 0.5% or ₹100
+                per executed order (whichever is lower), while PIS accounts are
+                charged 0.5% or ₹200 per executed order (whichever is lower).
+              </li>
+
+              <li>
+                Please note that if your account has a debit balance, orders
+                will be charged ₹40 per executed order instead of the standard
+                ₹20 rate
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-4 p-4 mt-5 ">
+             <a href="" style={{textDecoration:"none"}}>
+              <h3 className="text-center">List of Charges</h3>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Brokerage;
--- src/landing_page/pricing/Hero.jsx
+++ src/landing_page/pricing/Hero.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
-    return (
-        <>
-          <h1>Hero</h1>
-        </>
-    );
+  return (
+    <>
+      <div className="container mb-5 border-bottom">
+        <div className="text-center border-bottom mt-5 mb-5">
+          <h1>Pricing</h1>
+          <h4 className="text-muted mb-5 mt-3 fs-5">
+            Free Equity investments and flat ₹20 intraday and F&O trades
+          </h4>
+        </div>
+
+        <div className="row text-center">
+          <div className="col-4 p-4 ">
+            <img src="/media/images/pricingEquity.svg" alt="" />
+            <h1 className="fs-2">Free equity delivery</h1>
+            <p className="text-muted fs-5 mt-4">
+              All equity delivery investments (NSE, BSE) are absolutely free — ₹
+              0 brokerage.
+            </p>
+          </div>
+
+          <div className="col-4 p-4 ">
+            <img src="/media/images/intradayTrades.svg" alt="" />
+            <h1 className="fs-2">Intraday and F&O trades</h1>
+            <p className="text-muted fs-5 mt-4">
+             Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades.
+            </p>
+          </div>
+
+          <div className="col-4 p-4 ">
+            <img src="/media/images/pricingEquity.svg" alt="" />
+            <h1 className="fs-2">Free equity delivery</h1>
+            <p className="text-muted fs-5 mt-4">
+              All equity delivery investments (NSE, BSE) are absolutely free — ₹
+              0 brokerage.
+            </p>
+          </div>{" "}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
